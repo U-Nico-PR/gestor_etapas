55a8621f5842136e018c6c380a86c8e4161a5983
U-Nico-PR <195708604+U-Nico-PR@users.noreply.github.com>
2025-12-05 11:17:08 -0600

    Toda la estructura

---
+++ src/main/java/mx/uacm/gestor_etapas/GestorEtapasApplication.java
@@ -0,0 +1,13 @@
+package mx.uacm.gestor_etapas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestorEtapasApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GestorEtapasApplication.class, args);
+    }
+
+}
---
+++ src/test/java/mx/uacm/gestor_etapas/GestorEtapasApplicationTests.java
@@ -0,0 +1,13 @@
+package mx.uacm.gestor_etapas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestorEtapasApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
